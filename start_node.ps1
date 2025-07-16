cce3bf67621497be0fa05611fe396a04c2fafdda
Sergey Kolchin <skolchin@gmail.com>
2025-07-16 16:06:24 +0300

    upd

--- start_node.ps1
+++ start_node.ps1
@@ -16,13 +16,14 @@ else {
 
     ray start `
         --address "${Address}:6379" `
-        --node-ip-address $NodeAddress `
         --num-cpus 1 `
         --resources '{\"custom-resource\": 1}' `
         --node-manager-port 44403 `
         --object-manager-port 44404 `
         --runtime-env-agent-port 44405 `
-        --metrics-export-port 44406 `
+        --dashboard-agent-grpc-port 44406 `
+        --dashboard-agent-listen-port 44407 `
+        --metrics-export-port 44408 `
         --verbose
 }
 
