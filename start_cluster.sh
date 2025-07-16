721c8b9db18dd44f316c894cd055b8d851c2fee2
Sergey Kolchin <skolchin@gmail.com>
2025-07-16 12:16:18 +0300

    with ports

--- start_cluster.sh
+++ start_cluster.sh
@@ -16,6 +16,10 @@ else
         --num-cpus 1 \
         --node-ip-address=$1 \
         --node-manager-port 43403 \
+        --object-manager-port 43404 \
+        --runtime-env-agent-port 43405 \
+        --dashboard-agent-grpc-port 43406 \
+        --metrics-export-port 43407 \
         --dashboard-host 0.0.0.0 \
         --head \
         --verbose
--- start_node.sh
+++ start_node.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
-    ray start --num-cpus=1 --resources='{"custom-resource": 1}' --node-manager-port 43403
+    echo "Starting node locally"
+    ray start \
+        --num-cpus=1 \
+        --resources='{"custom-resource": 1}' \
+        --node-manager-port 43403 \
+        --verbose
 else
-    ray start --address=${1}:6379 --num-cpus=1 --resources='{"custom-resource": 1}' --node-manager-port 43403
+    echo "Starting node at ${1}"
+
+    ray start \
+        --address=${1}:6379 \
+        --num-cpus=1 \
+        --resources='{"custom-resource": 1}' \
+        --node-manager-port 43403 \
+        --object-manager-port 43404 \
+        --runtime-env-agent-port 43405 \
+        --dashboard-agent-grpc-port 43406 \
+        --metrics-export-port 43407 \
+        --verbose
+
 fi
 
 echo ""
