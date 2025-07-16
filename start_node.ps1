7dca614636d4e4e8b7d6a23e08d878a49cbe6fe1
Sergey Kolchin <skolchin@gmail.com>
2025-07-16 12:21:36 +0300

    fix

--- start_node.ps1
+++ start_node.ps1
@@ -16,7 +16,7 @@ else {
     ray start `
         --address "${Address}:6379" `
         --num-cpus 1 `
-        --resources '{"custom-resource": 1}' `
+        --resources '{\"custom-resource\": 1}' `
         --node-manager-port 43403 `
         --object-manager-port 43404 `
         --runtime-env-agent-port 43405 `
