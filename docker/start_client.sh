b102d1990134ba64041c07c93e6e531011f870d2
kol <skolchin@gmail.com>
2025-09-28 10:25:25 +0300

    wip

--- docker/start_client.sh
+++ docker/start_client.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Starting node of ${RAY_ADDRESS} cluster"
-ray start --block --address="${RAY_ADDRESS}:6379"
+echo "Starting node at ${RAY_ADDRESS} cluster"
+ray start \
+    --block \
+    --address=$RAY_ADDRESS \
+    --node-manager-port 44403 \
+    --object-manager-port 44404 \
+    --runtime-env-agent-port 44405 \
+    --dashboard-agent-grpc-port 44406 \
+    --dashboard-agent-listen-port 44407 \
+    --metrics-export-port 44408 \
+    --verbose
